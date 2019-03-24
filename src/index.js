1d9ddab9c2b190114a91d0d6160c98d38f38bd2f
xinwu.pxw <xinwu.pxw@alibaba-inc.com>
2019-03-24 18:36:29 +0800

    feat: 功能完善

--- src/index.js
+++ src/index.js
@@ -1,22 +1,54 @@
-import React, { Component } from 'react'
+import React from 'react'
+import Step from 'wuziqi-components-step'
 import PropTypes from 'prop-types'
+import './styles.css'
 
-import styles from './styles.css'
+const DIM = 10
 
-export default class ExampleComponent extends Component {
+class Board extends React.Component {
   static propTypes = {
-    text: PropTypes.string
+    squares: PropTypes.func,
+    onClick: PropTypes.func
+  }
+  renderSquare(i, j) {
+    return (
+      <Step
+        value={this.props.squares[i][j]}
+        onClick={() => this.props.onClick(i, j)}
+      />
+    )
+  }
+
+  renderSquareRow(i) {
+    let returnValue = []
+    for (let j = 0; j < DIM; j++) {
+      returnValue.push(<Step
+        value={this.props.squares[i][j]}
+        onClick={() => this.props.onClick(i, j)}
+        key={j}
+      />)
+    }
+    return returnValue
+  }
+
+  renderSquares() {
+    let returnValue = []
+    for (let i = 0; i < DIM; i++) {
+      returnValue.push(<div
+        className='board-row' key={i}>
+        {this.renderSquareRow(i)}
+      </div>)
+    }
+    return returnValue
   }
 
   render() {
-    const {
-      text
-    } = this.props
-
     return (
-      <div className={styles.test}>
-        Example Component: {text}
+      <div>
+        {this.renderSquares()}
       </div>
     )
   }
 }
+
+export default Board
